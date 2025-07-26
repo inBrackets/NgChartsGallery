75c4b7ef268155ac48e0837ab7e671a66c689453
Jason <extremewinds@gmail.com>
2025-07-26 09:29:35 +0200

    rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'AngularGitHubPages' title`, () => {
+  it(`should have the 'NgChartsGallery' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularGitHubPages');
+    expect(app.title).toEqual('NgChartsGallery');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, AngularGitHubPages');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NgChartsGallery');
   });
 });
