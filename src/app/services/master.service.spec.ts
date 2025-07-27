49b462aa48806936914e66010639c5606f5ec665
Jason <extremewinds@gmail.com>
2025-07-27 02:21:12 +0200

    fix tests by adding the providers
          providers: [
            provideHttpClient(),
            provideHighcharts()
          ],

--- src/app/services/master.service.spec.ts
+++ src/app/services/master.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MasterService } from './master.service';
+import {provideHttpClient} from '@angular/common/http';
 
 describe('MasterService', () => {
   let service: MasterService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient()
+      ],
+    });
     service = TestBed.inject(MasterService);
   });
 
